e351d7369aca7681f51cc0cd9c8286958cc0e7f1
Guilherme Fill <guilherme.fill@buddegroup.com>
2023-09-14 10:09:25 +0200

    addind data via template

--- pkg/pages/pages.go
+++ pkg/pages/pages.go
@@ -17,10 +17,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("public/html/base.html", "public/html/components/navigation.html", "public/html/pages/about.html")
+	data := map[string]interface{}{
+		"Title":   "About",
+		"Content": "This is the about page",
+	}
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, data)
 }
